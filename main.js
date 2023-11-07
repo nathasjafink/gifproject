82c028816b5f298c1f3c0549d04522b17df28978
nathasjafink <nathasja.nielsen@gmail.com>
2023-11-07 15:26:32 +0100

    js done for rendergifs and bottom

--- main.js
+++ main.js
@@ -1,21 +1,33 @@
 // sends a request to https://api.giphy.com/v1/gifs/search?api_key=2bTx4lXM5rIXS1vBbQ6LN129JQRNgViL&q=&limit=25
 const input = document.querySelector('#input-text');
+const output = document.querySelector('#output');
+let gifArray = [];
 async function fetchGifs (input) {
-    fetch('https://api.giphy.com/v1/gifs/search?api_key=2bTx4lXM5rIXS1vBbQ6LN129JQRNgViL&q=' + input + '&limit=25')
+    fetch('https://api.giphy.com/v1/gifs/search?api_key=2bTx4lXM5rIXS1vBbQ6LN129JQRNgViL&q=' + input + '&limit=10')
         .then(response => response.json())
         .then(gifData => {
-            console.log(gifData);
             // HERE IS WHERE YOU WRITE YOUR CODE!!!!!!!!
-
-
-
-
+            gifArray = [];
+            gifData.data.forEach(gif => {
+                gifArray.push(gif.images.original.url);
+            })
+            renderGifs()
         });
+}
+
+function renderGifs () {
+    output.innerHTML = '';
+
+    gifArray.forEach( (gif) => {
+        const img = document.createElement("img");
+        img.src = gif;
+        output.appendChild(img);
+    })
+
 }
 
 const submitBtn = document.querySelector('#submit-btn');
 
-    submitBtn.addEventListener('click', () => {
-        const output = document.querySelector('.output');
-
+submitBtn.addEventListener('click', () => {
+    fetchGifs(input.value);
 })
